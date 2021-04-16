0fed8decc4fe90ff2ab5f3c838d609b667db9cfa
Zachary Ho <zyyho97@gmail.com>
2021-04-17 01:12:27 +1000

    created react app

---
+++ flashkardz-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Flashkardz
+        </h1>
+        <p>
+          Making studying less dry
+        </p>
+        <a
+          className="App-link"
+          href="https://devpost.com/software/los-novatos-project-tbd?ref_content=my-projects-tab&ref_feature=my_projects"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Team Devpost
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
