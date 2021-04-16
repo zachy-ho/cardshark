94f61c00d4a62186fd25feb76d3276e7c46a9936
Zachary Ho <zyyho97@gmail.com>
2021-04-17 02:02:01 +1000

    testing updated husky

--- flashkardz-app/src/App.js
+++ flashkardz-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Flashkardzasdfsaa
+          Flashkardzasdfsaaa
         </h1>
         <p>
           Making studying less dry
