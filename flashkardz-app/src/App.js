94e450735c34169021e2333de1eb4f2d06a8936a
Zachary Ho <zyyho97@gmail.com>
2021-04-17 01:56:13 +1000

    testing husky and lint-staged

--- flashkardz-app/src/App.js
+++ flashkardz-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Flashkardz
+          Flashkardzasdfsa
         </h1>
         <p>
           Making studying less dry
