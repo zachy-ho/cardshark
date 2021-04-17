ae23b1c8cc75875540a3b3c01cb9a2cd0cd4144f
Zachary Ho <zyyho97@gmail.com>
2021-04-17 17:36:28 +1000

    merged with main

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import PrivateRoute from './components/routes/PrivateRoute';
 import Home from './components/pages/Home';
 import DeckView from './components/pages/DeckView';
 import './App.css';
-import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <Router>
         <Switch>
           <PrivateRoute path="/" component={Home} />
