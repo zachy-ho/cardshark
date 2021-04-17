32748ca65ac673de6d7be669956373420302be30
patawat watakul <patawat.wtk@gmail.com>
2021-04-18 02:35:48 +1000

    update topic page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import { Container } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import firebase from '../../firebase';
+import cardImage from '../../cardImage.jpg';
 
-const Home = () => (
-  <Container>
-    <Link to="/deck">
-      <h1>Hi</h1>
-    </Link>
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topics: [],
+    };
+    const useStyles = makeStyles({
+      root: {
+        maxWidth: 345,
+      },
+      media: {
+        height: 140,
+      },
+    });
+    this.classes = useStyles();
+  }
 
-  </Container>
-);
+  componentDidMount() {
+    const topicRef = firebase.firestore().collection('topics');
+    topicRef.get().then((snapshot) => {
+      const newTopics = [];
+      snapshot.forEach((doc) => {
+        newTopics.push(doc.data());
+        this.setState({
+          topics: newTopics,
+        });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Grid container spacing={4} maxWidth="md">
+          {this.state.topics.map((topic) => {
+            return (
+              <Grid item key={topic.id} xs={12} sm={6} md={4}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      className={this.classes.media}
+                      image={cardImage}
+                      title="Image title"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {topic.title}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    );
+  }
+}
 
 export default Home;
