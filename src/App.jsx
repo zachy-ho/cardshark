92412c9c56ea2f9124b15cfc619acfbc9076311b
Zachary Ho <zyyho97@gmail.com>
2021-04-17 15:07:55 +1000

    added link to deckview. not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/routes/PrivateRoute';
-// import Home from './components/Home';
+import Home from './components/pages/Home';
 import DeckView from './components/pages/DeckView';
 import './App.css';
 
@@ -10,7 +10,8 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <PrivateRoute path="/" component={DeckView} />
+          <PrivateRoute path="/" component={Home} />
+          <PrivateRoute path="/deck" component={DeckView} />
         </Switch>
       </Router>
     </div>
